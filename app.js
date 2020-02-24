55c03824b0558850be522b9d0776349ae48a2c92
sbaek2 <seunghyuk.baek@powerplan.com>
2020-02-24 13:41:50 -0500

    new

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ dotenv.config();
 mongoose
     .connect(
         process.env.MONGO_URI,
-        { useNewUrlParser: true }
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
     )
     .then(() => console.log("DB Connected"));
 
